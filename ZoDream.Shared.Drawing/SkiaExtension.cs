870ee95dd4a9508e57dd6800938e57cf287c1eec
ZoDream <648383079@qq.com>
2024-11-21 20:09:40 +0800

    增加图像边界获取

--- ZoDream.Shared.Drawing/SkiaExtension.cs
+++ ZoDream.Shared.Drawing/SkiaExtension.cs
@@ -1,8 +1,10 @@
 ﻿using SkiaSharp;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Numerics;
-using System.Reflection.Metadata;
+using System.Threading.Tasks;
 
 namespace ZoDream.Shared.Drawing
 {
@@ -106,14 +108,14 @@ namespace ZoDream.Shared.Drawing
         public static void SaveAs(this SKImage image, string fileName)
         {
             using var fs = File.OpenWrite(fileName);
-            using var pixmap = image.PeekPixels();
-            pixmap?.Encode(fs, ConvertFormat(fileName), 100);
+            using var pixMap = image.PeekPixels();
+            pixMap?.Encode(fs, ConvertFormat(fileName), 100);
         }
 
         public static void Encode(this SKImage image, Stream dst, SKEncodedImageFormat format, int quality)
         {
-            using var pixmap = image.PeekPixels();
-            pixmap?.Encode(dst, format, quality);
+            using var pixMap = image.PeekPixels();
+            pixMap?.Encode(dst, format, quality);
         }
 
         /// <summary>
@@ -200,9 +202,9 @@ namespace ZoDream.Shared.Drawing
         {
             var target = SKImage.Create(new SKImageInfo(rect.Width, 
                 rect.Height));
-            using var targetPixmap = target.PeekPixels();
-            using var imagePixmap = source.PeekPixels();
-            if (imagePixmap.ExtractSubset(targetPixmap, 
+            using var targetPixMap = target.PeekPixels();
+            using var imagePixMap = source.PeekPixels();
+            if (imagePixMap.ExtractSubset(targetPixMap, 
                 rect))
             {
                 return target;
@@ -210,6 +212,12 @@ namespace ZoDream.Shared.Drawing
             target.Dispose();
             return null;
         }
+
+        public static SKBitmap Mutate(SKRect rect, Action<SKCanvas> action)
+        {
+            return Mutate((int)rect.Width, (int)rect.Height, action);
+        }
+
         public static SKBitmap Mutate(int width, 
             int height, 
             Action<SKCanvas> action)
@@ -255,14 +263,158 @@ namespace ZoDream.Shared.Drawing
                 return null;
             }
             var target = SKImage.Create(info);
-            using var targetPixmap = target.PeekPixels();
-            using var imagePixmap = image.PeekPixels();
-            if (imagePixmap.ScalePixels(targetPixmap, quality))
+            using var targetPixMap = target.PeekPixels();
+            using var imagePixMap = image.PeekPixels();
+            if (imagePixMap.ScalePixels(targetPixMap, quality))
             {
                 return target;
             }
             target.Dispose();
             return null;
+        }
+        /// <summary>
+        /// 获取图片上所有物体轮廓
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static SKPath[] GetObject(this SKBitmap image)
+        {
+            using var imagePixMap = image.PeekPixels();
+            return GetObject(imagePixMap);
+        }
+        /// <summary>
+        /// 获取图片上所有物体轮廓
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static Task<SKPath[]> GetObjectAsync(this SKBitmap image)
+        {
+            var imagePixMap = image.PeekPixels();
+            return Task.Factory.StartNew(() => {
+                try
+                {
+                    return GetObject(imagePixMap);
+                } finally
+                {
+                    imagePixMap.Dispose();
+                }
+            });
+        }
+        /// <summary>
+        /// 获取图片上所有物体轮廓
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static SKPath[] GetObject(this SKImage image)
+        {
+            using var imagePixMap = image.PeekPixels();
+            return GetObject(imagePixMap);
+        }
+
+        /// <summary>
+        /// 获取所有物体的区域
+        /// </summary>
+        /// <param name="pixMap"></param>
+        /// <returns></returns>
+        public static SKPath[] GetObject(SKPixmap pixMap)
+        {
+            var items = new List<SKPath>();
+            for (var i = 0; i < pixMap.Height; i++)
+            {
+                for (var j = 0; j < pixMap.Width; j++)
+                {
+                    if (IsTransparent(pixMap, j, i) || Contains(items, j, i))
+                    {
+                        continue;
+                    }
+                    var path = TraceContour(pixMap, j, i);
+                    if (path is null)
+                    {
+                        continue;
+                    }
+                    items.Add(path);
+                }
+            }
+            return [.. items];
+        }
+
+        private static bool Contains(IEnumerable<SKPath> items, int x, int y)
+        {
+            foreach (var item in items)
+            {
+                if (item.Contains(x, y))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool IsTransparent(SKPixmap pixMap, int x, int y)
+        {
+            if (x < 0 || y < 0)
+            {
+                return true;
+            }
+            return pixMap.GetPixelColor(x, y).Alpha == 0;
+        }
+
+        /// <summary>
+        /// 边界算法
+        /// </summary>
+        /// <param name="pixMap"></param>
+        /// <param name="begin"></param>
+        /// <returns></returns>
+        private static SKPath? TraceContour(SKPixmap pixMap, int beginX, int beginY)
+        {
+            var path = new SKPath();
+            path.MoveTo(beginX, beginY);
+            var directItems = new int[][] {
+                [0, -1], [1, -1], 
+                         [1, 0],
+                [1, 1], [0, 1], [-1, 1],
+                [-1, 0], [-1, -1]
+            };
+            var beginDirect = 0;
+            var isBegin = false;
+            var curX = beginX;
+            var curY = beginY;
+            while (!isBegin)
+            {
+                var i = 0;
+                var direct = beginDirect;
+                var hasPoint = false;
+                while (i ++ <= directItems.Length)
+                {
+                    var x = curX + directItems[direct][0];
+                    var y = curY + directItems[direct][1];
+                    if (IsTransparent(pixMap, x, y))
+                    {
+                        direct = (direct + 1) % directItems.Length;
+                        continue;
+                    }
+                    hasPoint = true;
+                    curX = x;
+                    curY = y;
+                    if (curX == beginX && curY == beginY)
+                    {
+                        isBegin = true;
+                        path.Close();
+                    }
+                    else
+                    {
+                        path.LineTo(curX, curY);
+                    }
+                    beginDirect = (direct + 5) % directItems.Length;
+                    break;
+                }
+                if (!hasPoint)
+                {
+                    // 所有方向都没有结束
+                    return null;
+                }
+            }
+            return path;
         }
 
         private static bool LineIsIntersecting(SKPoint aBegin, SKPoint aEnd,
